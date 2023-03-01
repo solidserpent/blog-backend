61352e6788eaed66e07449c82f2c33eabe2987db
solidserpent <37921848+solidserpent@users.noreply.github.com>
2023-03-01 18:07:17 -0500

    Updated db connection process

--- db/db.js
+++ db/db.js
@@ -2,11 +2,23 @@ import Sequelize from "sequelize";
 import PostModel from "./Post.js";
 import UserModel from "./User.js";
 
-const db = new Sequelize("postgres://bkh@localhost:5432/blog", {
-  logging: false,
-});
-const Post = PostModel(db);
-const User = UserModel(db);
+let db;
+if (process.env.RDS_HOSTNAME) {
+  console.log("Connecting to RDS", process.env.RDS_HOSTNAME);
+  // if we're running on elasticbeanstalk, use elasticbeanstalk connection
+  db = new Sequelize(
+    `postgres://${process.env.RDS_USERNAME}:${process.env.RDS_PASSWORD}@${process.env.RDS_HOSTNAME}:${process.env.RDS_PORT}/${process.env.RDS_DB_NAME}`,
+    {
+      logging: false,
+    }
+  );
+} else {
+  console.log("Connecting to local database");
+  // if we're running locally, use the localhost connection
+  db = new Sequelize("postgres://bkh@localhost:5432/blog", {
+    logging: false,
+  });
+}
 
 const connectToDB = async () => {
   try {
